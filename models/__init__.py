78dfa69859adb33fb6426a294b5ed61f3743d1a3
rneyns <rneyns@outlook.com>
2024-07-24 10:11:17 +0200

    Added the models folder

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 11:21:27 2023
+
+@author: Robbe Neyns
+"""
+
+from models.pretrainmodel import SAINT
+from models.pretrainmodel_vision import SAINT_vision
